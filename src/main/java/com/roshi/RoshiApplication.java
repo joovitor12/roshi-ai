cad9a517e3f90fd06202896684efae36804a2d32
Joao Vitor <outlast4321@gmail.com>
2024-05-10 19:20:34 -0300

    feat: init project

---
+++ src/main/java/com/roshi/RoshiApplication.java
@@ -0,0 +1,13 @@
+package com.roshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  RoshiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoshiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roshi/RoshiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roshi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoshiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
